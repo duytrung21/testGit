c3796fe34ab44e5b751b75fae39c02da553d49bf
Xin chao mn <mactrung116@gmail.com>
2025-04-28 10:18:48 +0700

    update code

--- testGit/Controllers/CongTyController.cs
+++ testGit/Controllers/CongTyController.cs
@@ -10,13 +10,37 @@ namespace testGit.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class CongTyController : ControllerBase
-    {       
-        
+    {
+        private readonly string connectionString = "Data Source=DESKTOP-GRA8A3I;Initial Catalog=TestAPI_ADO;Integrated Security=True";
         [HttpPost]
         public IActionResult GetAll()
         {
-            
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "Select ID, Name from Employees";
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            employees.Add(new Employee
+                            {
+                                ID = reader.GetInt32(0),
+                                Name = reader.GetString(1)
+                            });
+                        }
+                    }
+                }
+            }
             return Ok();
+        }
+        public class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
         }
     }
 }
