d4889525a3e374370268f997342244fbb7cc2df0
Xin chao mn <mactrung116@gmail.com>
2025-04-28 09:35:48 +0700

    Cap nhat lai code

---
+++ testGit/Controllers/CongTyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace testGit.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CongTyController : ControllerBase
+    {       
+        
+        [HttpPost]
+        public IActionResult GetAll()
+        {
+            
+            return Ok();
+        }
+    }
+}
---
+++ testGit/Data/ConnectionSqlByADO.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testGit.Data
+{
+    public class ConnectionSqlByADO
+    {
+        private readonly string connectionString = "Data Source=DESKTOP-GRA8A3I;Initial Catalog=TestAPI_ADO;Integrated Security=True";
+        public void CreateTable()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                //Tao bang Employees
+                string createEmployeesTable = @"
+                    if object_ID('Employees','U') is null
+                    begin
+                        create table Employees(     
+                            EmployeeID int primary key identity(1,1),
+                            Name nvarchar(100) not null);
+                    end";
+                using (SqlCommand cmd = new SqlCommand(createEmployeesTable, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return;
+        }
+    }
+}
